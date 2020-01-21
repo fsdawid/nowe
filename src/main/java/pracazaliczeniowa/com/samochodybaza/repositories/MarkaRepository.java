0925b54f54cdd769ec12bc32694a94aa0730d446
Piotr Pasich <piotr.pasich@gmail.com>
2020-01-21 08:02:27 +0100

    repo

---
+++ src/main/java/pracazaliczeniowa/com/samochodybaza/repositories/MarkaRepository.java
@@ -0,0 +1,7 @@
+package pracazaliczeniowa.com.samochodybaza.repositories;
+
+import pracazaliczeniowa.com.samochodybaza.model.Marka;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MarkaRepository extends CrudRepository<Marka, Long> {
+}
