512ef1402a9231626e130325853b8c3ce5605b11
fsdawid <59917302+fsdawid@users.noreply.github.com>
2020-01-25 00:37:19 +0100

    Add files via upload

--- listajednokierunkowa.cpp
+++ listajednokierunkowa.cpp
@@ -1,131 +1,143 @@
 #include <iostream>
-#include <string>
-using namespace std;
+ using namespace std;
 
+ struct slistEl
+ {
+ slistEl * next;
+ int data;
+ };
+ class stack
+ {
+ private:
+ slistEl * S; 
+ public:
+ stack(); 
+ ~stack();
+ bool empty(void);
+ slistEl * top(void);
+ void push(int v);
+ void pop(void);
+ };
+ stack::stack()
+ {
+ S = NULL;
+ }
+ stack::~stack()
+ {
+ while(S) pop();
+ }
+ bool stack::empty(void)
+ {
+ return !S;
+ }
+ slistEl * stack::top(void)
+ {
+ return S;
+ }
 
-void insert_end(int x);
-void display_forward();
-void insert_front(int x);
-void delete_front();
-void delete_end();
-struct node
-{
-int data;
-node *prev;
-node *next;
-};
-node *head = NULL, *tail = NULL;
+ void stack::push(int v)
+ {
+ slistEl * e = new slistEl;
+ e->data = v;
+ e->next = S;
+ S = e;
+ }
 
+ void stack::pop(void)
+ {
+ if(S)
+ {
+ slistEl * e = S;
+ S = S->next;
+ delete e;
+ }
+ }
 
-
-
-
-int main()
-{
-    insert_end(8);
-    insert_end(7);
-    insert_end(6);
-    insert_end(5);
-    insert_end(4);
-    insert_end(3);
-    display_forward();
-    cout << endl;
-    insert_front(1);
-    insert_front(2);
-    insert_front(3);
-    insert_front(4);
-    insert_front(5);
-    insert_front(6);
-    display_forward();
-    delete_front();
-    cout << endl;
-    display_forward();
-    cout << endl;
-    delete_end();
-    display_forward();
-    
-    
-    
-    return 0;    
-}
-
-
-void display_forward()//Przeglad listy
-{
-node *temp;
-if(head==NULL)
-cout <<"No data inside\n";
-else
-{
-temp = head;
-while(temp!=NULL)
-{
-cout << temp->data << endl;
-temp = temp->next;
-}
-}
-}
-
-
-
-
-void insert_end(int x)//Dodawanie na koniec
-{
-node* temp = new node;
-temp->data = x;
-temp->next = NULL;
-temp->prev = NULL;
-if (head == NULL)
-head = tail = temp;
-else {
-tail->next = temp;
-temp->prev = tail;
-tail = temp;
-}
-}
-
-void insert_front(int x)//Dodawanie na pocz¹tek
-{
-node* temp = new node;
-temp->data = x;
-temp->next = NULL;
-temp->prev = NULL;
-if (head == NULL)
-head = tail = temp;
-else
-{
-temp->next = head;
-head->prev = temp;
-head = temp;
-}
-}
-
-
-void delete_front()//Usuwanie z pocz¹tku
-{
-node *temp;
-if(head==NULL)
-cout <<"No data inside\n";
-else
-{
-temp = head;
-head = head->next;
-head->prev = NULL;
-delete temp;
-}
-}
+ int main()
+ {
+ stack S;
+ int i;
+ for(i = 1; i <= 10; i++) S.push(i);
+ while(!S.empty())
+ {
+ cout << S.top()->data << endl;
+ S.pop();
+ }
+ } 
 
 
 
-void delete_end()//Usuniêcie z koñca
-{
-node *temp;
-if(tail==NULL)
-cout <<"No data inside\n";
-else
-{
-temp = tail;
-tail = tail->prev;
-tail->next = NULL;
-delete temp;
-}
+Implementacja kolejki za pomoc¹ listy
+#include <iostream>
+ using namespace std;
+ const int MAXINT = -2147483647;
+ struct slistEl
+ {
+ slistEl * next;
+ int data;
+ };
+ class queue
+ {
+ private:
+ slistEl * head;
+ slistEl * tail;
+ public:
+ queue(); 
+ ~queue(); 
+ bool empty(void);
+ int front(void);
+ void push(int v);
+ void pop(void);
+ };
+queue::queue()
+ {
+ head = tail = NULL;
+ }
+ queue::~queue()
+ {
+ while(head) pop();
+ }
+
+ bool queue::empty(void)
+ {
+ return !head;
+ }
+
+ int queue::front(void)
+ {
+ if(head) return head->data;
+ else return -MAXINT;
+ }
+
+ void queue::push(int v)
+ {
+ slistEl * p = new slistEl;
+ p->next = NULL;
+ p->data = v;
+ if(tail) tail->next = p;
+ else head = p;
+ tail = p;
+ }
+
+ void queue::pop(void)
+ {
+ if(head)
+ {
+ slistEl * p = head;
+ head = head->next;
+ if(!head) tail = NULL;
+ delete p;
+ }
+ }
+
+ int main()
+ {
+ queue Q; 
+ int i;
+ for(i = 1; i <= 10; i++) Q.push(i);
+ while(!Q.empty())
+ {
+ cout << Q.front() << endl;
+ Q.pop();
+ }
+ } 
