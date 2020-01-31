6bbf868d6bea082bf681e20bea411827f8941cbf
fsdawid <59917302+fsdawid@users.noreply.github.com>
2020-01-31 19:10:13 +0100

    Add files via upload

--- warcabynew/warcaby dawid grabowski/Form1.cs
+++ warcabynew/warcaby dawid grabowski/Form1.cs
@@ -50,7 +50,7 @@ namespace warcaby_dawid_grabowski
             rojas.Add(roja10);
             rojas.Add(roja11);
             rojas.Add(roja12);
-  
+
         }
 
         public void seleccion(object objeto)
@@ -71,14 +71,15 @@ namespace warcaby_dawid_grabowski
         {
             if (seleccionado != null)
             {
-                if (true)
+                string color = seleccionado.Name.ToString().Substring(0, 4);
+                if (validaction(seleccionado, cuadro, color))
                 {
-                    string color = seleccionado.Name.ToString().Substring(0, 4);
+
                     Point anterior = seleccionado.Location;
                     seleccionado.Location = cuadro.Location;
                     int avance = anterior.Y - cuadro.Location.Y;
 
-                    if (true)
+                    if (!movimientosExtras(color) | Math.Abs(avance) == 50)
                     {
                         ifqueen(color);
                         turno++;
@@ -93,11 +94,97 @@ namespace warcaby_dawid_grabowski
                     }
                 }
             }
+        } 
+
+        private bool movimientosExtras(string color)
+        {
+            List<PictureBox> bandoContrario = color == "roja" ? azules : rojas;
+            List<Point> posiciones = new List<Point>();
+            int sigPosicion = color == "roja" ? -100 : 100;
+            posiciones.Add(new Point(seleccionado.Location.X + 100, seleccionado.Location.Y + sigPosicion));
+            posiciones.Add(new Point(seleccionado.Location.X - 100, seleccionado.Location.Y + sigPosicion));
+            if (seleccionado.Tag == "queen")
+            {
+                posiciones.Add(new Point(seleccionado.Location.X + 100, seleccionado.Location.Y - sigPosicion));
+                posiciones.Add(new Point(seleccionado.Location.X - 100, seleccionado.Location.Y - sigPosicion));
+            }
+
+            bool resultado = false;
+            for (int i = 0; i < posiciones.Count; i++)
+            {
+                if (posiciones[i].X >= 50 && posiciones[i].X <= 400 && posiciones[i].Y >= 50 && posiciones[i].Y <= 400)
+                {
+                    if (!ocupado(posiciones[i], rojas) && !ocupado(posiciones[i], azules))
+                    {
+                        Point puntoMedio = new Point(promedio(posiciones[i].X, seleccionado.Location.X), promedio(posiciones[i].Y, seleccionado.Location.Y));
+                        if (ocupado(puntoMedio, bandoContrario))
+                        {
+                            resultado = true;
+
+                        }
+                    }
+                }
+            }
+            return resultado;
+        }
+
+        private bool ocupado(Point punto, List<PictureBox> bando)
+        {
+            for (int i = 0; i < bando.Count; i++)
+            {
+                if (punto == bando[i].Location)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private int promedio(int n1, int n2)
+        {
+            int resultado = n1 + n2;
+            resultado = resultado / 2;
+            return Math.Abs(resultado);
+        }
+
+        private bool validaction(PictureBox origen, PictureBox destino, string color)
+        {
+            Point puntoOrigen = origen.Location;
+            Point puntoDestino = destino.Location;
+            int avance = puntoOrigen.Y - puntoDestino.Y;
+            avance = color == "reja" ? avance : (avance * -1);
+            avance = seleccionado.Tag == "queen" ? Math.Abs(avance) : avance;
+
+            if (avance == 50)
+            {
+                return true;
+            }
+            else if (avance == 100)
+            {
+                Point puntoMedio = new Point(promedio(puntoDestino.X, puntoOrigen.X), promedio(puntoDestino.Y, puntoOrigen.Y));
+                List<PictureBox> bandoContrario = color == "roja" ? azules : rojas;
+                for (int i = 0; i < bandoContrario.Count; i++)
+                {
+                    if (bandoContrario[i].Location == puntoMedio)
+                    {
+                        bandoContrario[i].Location = new Point(0, 0);
+                        bandoContrario[i].Visible = false;
+                        return true;
+                    }
+                }
+
+
+            }
+            return false; 
+           
+        
+
+.
         }
 
         private void ifqueen(string color)
         {
-            if(color=="azul" && seleccionado.Location.Y == 400)
+            if (color == "azul" && seleccionado.Location.Y == 400)
             {
                 seleccionado.BackgroundImage = Properties.Resources.Bez_nazwy_2kpng;
                 seleccionado.Tag = "queen";
@@ -121,3 +208,5 @@ namespace warcaby_dawid_grabowski
         }
     }
 }
+
+
